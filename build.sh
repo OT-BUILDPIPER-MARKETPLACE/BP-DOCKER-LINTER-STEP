4eb5f891dd8cce50147947db011e70d7c903c614
Sandeep <sandeep@opstree.com>
2022-10-20 16:18:38 +0530

    Adding halolint for dockerfile analysis

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 source functions.sh
 
-logInfoMessage "I'll scan the Dockerfile  available at [${WORKSPACE}/${CODEBASE_DIR}]"
+logInfoMessage "I'll scan the Dockerfile  available at [${WORKSPACE}${CODEBASE_DIR}]"
+
+sleep  $SLEEP_DURATION
+cd  $WORKSPACE/${CODEBASE_DIR}
+
+hadoling ${DOCKERFILE_PATH}
