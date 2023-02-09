5eac02ac2e14f6e051f382938fc55904275d79fb
Priyanshi Chauhan <priyanshi.chauhan@opstree.com>
2023-02-09 18:19:04 +0530

    changed the logic

--- build.sh
+++ build.sh
@@ -11,18 +11,17 @@ logInfoMessage "I'll scan the Dockerfile  available at [$WORKSPACE/$CODEBASE_DIR
 sleep  $SLEEP_DURATION
 cd  $WORKSPACE/${CODEBASE_DIR}
 
-hadolint ${DOCKERFILE_PATH}
-
 if [ -f "$DOCKERFILE_PATH" ]
 then
+  hadolint $DOCKERFILE_PATH
   logInfoMessage "Congratulations docker lint scan succeeded!!!"
   generateOutput mvn_execute true "Congratulations docker lint scan succeeded!!!"
 elif [ "$VALIDATION_FAILURE_ACTION" == "FAILURE" ]
   then
     logErrorMessage "Please check docker lint scan failed!!!"
-    generateOutput ${ACTIVITY_SUB_TASK_CODE} false "Please check docker lint scan failed!!!"
+    generateOutput $ACTIVITY_SUB_TASK_CODE false "Please check docker lint scan failed!!!"
     exit 1
    else
     logWarningMessage "Please check docker lint scan failed!!!"
-    generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Please check docker lint scan failed!!!"
+    generateOutput $ACTIVITY_SUB_TASK_CODE true "Please check docker lint scan failed!!!"
 fi
